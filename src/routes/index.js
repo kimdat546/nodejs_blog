564946f97daf81452106a9d1537d3f9cca90084e
kimdat546 <kimdat546@gmail.com>
2021-05-23 10:12:55 +0700

    install prettier lint staged husky

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const newsRoute = require('./news');
+const siteRoute = require('./site');
+const siteController = require('../app/controllers/SiteController');
+
+function routes(app) {
+    app.use('/news', newsRoute);
+    app.use('/', siteRoute);
+}
+module.exports = routes;
